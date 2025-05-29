1d35b2020c490ecd6fba73c93e45bea22f5fac08
JoseCorellaVega <jose.corella.vega@udla.edu.ec>
2025-05-29 16:43:20 -0500

    agrego clases

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -1,2 +1,39 @@
 public class Cuenta {
+    protected double saldo;
+    protected int numDepositos;
+    protected int numRetiros;
+
+    public Cuenta() {
+        saldo = 0;
+        numDepositos = 0;
+        numRetiros = 0;
+    }
+
+    public void depositar(double monto) {
+        if (monto > 0) {
+            saldo += monto;
+            numDepositos++;
+        }
+    }
+
+    public boolean retirar(double monto) {
+        if (monto > 0 && saldo >= monto) {
+            saldo -= monto;
+            numRetiros++;
+            return true;
+        }
+        return false;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public int getNumDepositos() {
+        return numDepositos;
+    }
+
+    public int getNumRetiros() {
+        return numRetiros;
+    }
 }
